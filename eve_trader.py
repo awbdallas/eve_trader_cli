5da88a3ffd5923a9929ce83fdc67854bf640d097
Austin Briggs <Awbdallas@gmail.com>
2016-10-31 20:28:10 -0500

    Just putting here to move to other computer

--- eve_trader.py
+++ eve_trader.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import sys
 import argparse
+import requests
 
 """
 """
@@ -14,14 +16,27 @@ def main():
 
     # File or item is fine
     if args.system and args.item or args.system and args.file:
-        items = load_tems()
+        eve_items = load_eve_items()
+
+        if args.file:
+            input_items = load_user_items(args.file, eve_items)
+        else:
+            input_items = args.item
+            
+            # TODO add support for systems
+            # TODO convert for multiple items
+            if not eve_items.get(args.item, None):
+                print("Item {} is not a valid item".format(args.item))
+                sys.exit() 
+        
+        get_item_prices(input_items, eve_items, args.system)
 
 
 """
 Purpose: Load all the items from the eve_items file into a dict
 Returns: eve items in dict form. Keys can be either number or name
 """
-def load_items():
+def load_eve_items():
     file_name = "./eve_items.csv"
     items = {}
     with open('filename', 'r') as items_fh:
@@ -43,7 +58,16 @@ def load_items():
     return items
 
 
+def get_item_prices(input_items, eve_items, system_id):
+    return None
+
+
 def make_url(items, system):
+    base_url = "http://api.eve-central.com/api/marketstat?"
+    # I believe the item limit is about 100 per request
+    chunks = [items[i:i+100] for i in range(0, len(items), 100)]
+
+
     return None
 
 
