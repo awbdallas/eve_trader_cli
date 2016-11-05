dada64689242ef8ca8d15528a77ef3bd4ec3c11f
Austin <awbdallas@gmail.com>
2016-11-05 18:25:31 -0500

    CLI working now, able to get single item queries working correctly

--- eve_trader.py
+++ eve_trader.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
-import sys
 import argparse
+import json
 import requests
+import sys
+import xml.etree.ElementTree as ET
+from terminaltables import AsciiTable
+
 
 """
+TODO's for the program
+Get basic command for one item working for query
+Do it for large ones
+Create a "report" that will have things like shipping costs
+and things like that. Hopefully outputting to a spreadsheet of some sort
 """
 def main():
 
@@ -21,17 +30,66 @@ def main():
         if args.file:
             input_items = load_user_items(args.file, eve_items)
         else:
-            input_items = args.item
+            input_items = [args.item]
             
             # TODO add support for systems
             # TODO convert for multiple items
             if not eve_items.get(args.item, None):
                 print("Item {} is not a valid item".format(args.item))
-                sys.exit() 
+                sys.exit()
         
-        get_item_prices(input_items, eve_items, args.system)
+        eve_items = get_item_prices(input_items, eve_items, args.system)
+        display_info(input_items, eve_items)
+    else:
+        print("Please have a system and item, or system and a file")
+
+def display_info(input_items, eve_items):
+    base_table = [['Item', 'Buy Max', 'Sell Min']]
+    for item in input_items:
+        base_table.append([
+            eve_items[item]['name'],
+            convert_number(eve_items[item]['market_info']['buy']['avg']),
+            convert_number(eve_items[item]['market_info']['sell']['avg'])
+            ])
+
+    table = AsciiTable(base_table)
+    print(table.table)
 
 
+"""
+Purpose: convert numbers to be easier to digest
+Returns: Formatted double
+"""
+
+def convert_number(input_number):
+    # billions
+    if input_number / 1e9 > 1:
+        return "%.2fB" % round((input_number / 1e9), 2)
+
+    # millions
+    elif input_number / 1e6 > 1:
+        return "%.2fM" % round((input_number / 1e6), 2)
+    # thousands
+    elif input_number / 1e3 > 1:
+        return "%.2fK" % round((input_number / 1e3), 2)
+    # everything that's left
+    else:
+        return "%.2f" % round(input_number, 2)
+
+    return None
+
+
+# TODO add checking in the future
+def load_user_items(file_input, eve_items):
+    user_items = []
+
+    with open(file_input, 'r') as user_file:
+        for line in user_file:
+            line = line.strip()
+            if eve_items.get(line, None):
+                user_items.append(line)
+
+    return user_items 
 """
 Purpose: Load all the items from the eve_items file into a dict
 Returns: eve items in dict form. Keys can be either number or name
@@ -39,7 +97,7 @@ Returns: eve items in dict form. Keys can be either number or name
 def load_eve_items():
     file_name = "./eve_items.csv"
     items = {}
-    with open('filename', 'r') as items_fh:
+    with open(file_name, 'r') as items_fh:
         for line in items_fh:
             # itemid, groupid, name, volume
             line = line.split(',')
@@ -58,21 +116,37 @@ def load_eve_items():
     return items
 
 
+"""
+"""
 def get_item_prices(input_items, eve_items, system_id):
-    return None
+    urls = make_url(input_items, system_id)
+    for url in urls:
+        response = requests.post(url)
+        response_list = json.loads(response.text)
+       
+        for item in response_list:
+            current_item = str(item['buy']['forQuery']['types'][0])
+            eve_items[current_item]['market_info'] = item
+
+    return eve_items
 
 
+"""
+Purpose: Make the urls for the actual request
+Returns: A list of urls
+"""
 def make_url(items, system):
-    base_url = "http://api.eve-central.com/api/marketstat?"
+    base_url = "http://api.eve-central.com/api/marketstat/json?"
+    urls = []
+
     # I believe the item limit is about 100 per request
     chunks = [items[i:i+100] for i in range(0, len(items), 100)]
+    
+    for chunk in chunks:
+         urls.append(base_url + '&'.join('typeid=%s' % x for x in chunk)\
+                 + '&usesystem=%s' % system)
+    return urls
 
-
-    return None
-
-
-def xml_to_dict():
-    return None
 
 
 if __name__ == '__main__':
