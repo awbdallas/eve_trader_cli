5bc175f91ee3546798bcaa1fa1132b4a767e823b
awbdallas <Austin.W.Briggs@gmail.com>
2016-11-07 10:45:05 -0700

    Added shipping feature

--- eve_trader.py
+++ eve_trader.py
@@ -23,10 +23,34 @@ def main():
     parser.add_argument("--item", help="Item in question", nargs="+",
                         action="append")
     parser.add_argument("--file", help="File of items")
+    parser.add_argument("--shipping", help="System From, System too, Shipping Cost",
+                        nargs="+", action="append")
     args = parser.parse_args()
 
     # File or item is fine
-    if args.system and args.item or args.system and args.file:
+    if (args.shipping and args.file) or (args.shipping and args.item):
+        eve_items = load_eve_items()
+        if args.file:
+            input_items = load_user_items(args.file, eve_items)
+        else:
+            input_items = []
+
+            for item in args.item[0]:
+                if not eve_items.get(item, None):
+                    print("Item {} is not a valid item".format(item))
+                    sys.exit()
+                else:
+                    input_items.append(eve_items[item]['itemid'])
+        from_system = args.shipping[0][0]
+        to_system = args.shipping[0][1]
+        price = int(args.shipping[0][2])
+
+        eve_items = get_item_prices(input_items, eve_items, 
+                [from_system, to_system])
+        display_shipping_info(input_items, eve_items, 
+                [from_system, to_system], price)
+
+    elif (args.system and args.item) or (args.system and args.file):
         eve_items = load_eve_items()
 
         if args.file:
@@ -43,13 +67,44 @@ def main():
                 else:
                     input_items.append(eve_items[item]['itemid'])
 
-        eve_items = get_item_prices(input_items, eve_items, args.system)
-        display_info(input_items, eve_items)
+        eve_items = get_item_prices(input_items, eve_items, [args.system])
+        display_market_info(input_items, eve_items, args.system)
     else:
         print("Please have a system and item, or system and a file")
 
 
-def display_info(input_items, eve_items):
+
+def display_shipping_info(input_items, eve_items, systems, shipping_cost):
+    base_table = [['Item', 'From System Sell', 'To System Sell', 'Shipping Cost',
+        'Difference (%)', 'Difference (isk)', 'To System Volume']]
+
+    for item in input_items:
+        name = eve_items[item]['name']
+        sell_from = eve_items[item][systems[0]]['market_info']['sell']['min']
+        sell_to = eve_items[item][systems[1]]['market_info']['sell']['min']
+        shipping = float(eve_items[item]['volume']) * shipping_cost
+        difference_isk = sell_to - sell_from
+        difference_percent = 100 * ((sell_to - sell_from) / sell_to)
+        volume_to = eve_items[item][systems[1]]['market_info']['sell']['volume']
+
+
+        base_table.append([
+            name,
+            convert_number(sell_from),
+            convert_number(sell_to),
+            convert_number(shipping),
+            convert_number(difference_percent) + '%',
+            convert_number(difference_isk),
+            convert_number(volume_to)
+            ])
+
+    table = AsciiTable(base_table)
+    print(table.table)
+
+    return None
+
+
+def display_market_info(input_items, eve_items, system):
     """
     Purpose: Displaying the info to terminal to make it a little cleaner
     Returns: None (default)
@@ -61,8 +116,8 @@ def display_info(input_items, eve_items):
     base_table = [['Item', 'Buy Max', 'Sell Min', 'Spread', 'Isk']]
     for item in input_items:
         name = eve_items[item]['name']
-        buy = eve_items[item]['market_info']['buy']['wavg']
-        sell = eve_items[item]['market_info']['sell']['wavg']
+        buy = eve_items[item][system]['market_info']['buy']['wavg']
+        sell = eve_items[item][system]['market_info']['sell']['wavg']
         spread = 100 * ((sell - buy) / sell)
         spread_isk = (sell - buy)
 
@@ -85,14 +140,14 @@ def convert_number(input_number):
     """
 
     # billions
-    if input_number / 1e9 > 1:
+    if input_number / 1e9 > 1 or input_number / 1e9 < -1:
         return "%.2fB" % round((input_number / 1e9), 2)
 
     # millions
-    elif input_number / 1e6 > 1:
+    elif input_number / 1e6 > 1 or input_number / 1e6 < -1:
         return "%.2fM" % round((input_number / 1e6), 2)
     # thousands
-    elif input_number / 1e3 > 1:
+    elif input_number / 1e3 > 1 or input_number / 1e3 < -1:
         return "%.2fK" % round((input_number / 1e3), 2)
     # everything that's left
     else:
@@ -125,6 +180,7 @@ def load_eve_items():
     with open(file_name, 'r') as items_fh:
         for line in items_fh:
             # itemid, groupid, name, volume
+            line = line.strip()
             line = line.split(',')
             items[line[0]] = {
                     'itemid': line[0],
@@ -141,15 +197,21 @@ def load_eve_items():
     return items
 
 
-def get_item_prices(input_items, eve_items, system_id):
-    urls = make_url(input_items, system_id)
+def get_item_prices(input_items, eve_items, system_ids):
+    urls = []
+    # Accounting for as many systems as allowed. 
+    for system in system_ids:
+        urls += make_url(input_items, system)
+
     for url in urls:
         response = requests.post(url)
         response_list = json.loads(response.text)
        
         for item in response_list:
+            system = str(item['buy']['forQuery']['systems'][0])
             current_item = str(item['buy']['forQuery']['types'][0])
-            eve_items[current_item]['market_info'] = item
+            eve_items[current_item][system] = {}
+            eve_items[current_item][system]['market_info'] = item
 
     return eve_items
 
